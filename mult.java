fad9c0030f1400032dc892a4f3cab3b6421e0ca3
manduakash <50767686+manduakash@users.noreply.github.com>
2022-06-22 14:27:01 +0530

    Create mult.java

---
+++ mult.java
@@ -0,0 +1,11 @@
+//MULTIPLICATION OF TWO NUMBERS
+class mult{
+	public static void main(String[] args) {
+	  int a=3; //1st number
+	  int b=5; //2nd number
+          int c= a*b; //Multiplication
+		
+	    //output
+ 	  System.out.println("a =" + a);
+ 	  System.out.println("b = " + b);
+	  System.out.println("Multiplication of A and B = " + c);
