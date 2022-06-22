e650e4b20d4cd9e3e5e76c946782790f2922ead5
manduakash <50767686+manduakash@users.noreply.github.com>
2022-06-22 14:02:00 +0530

    Create Cjava2.java

---
+++ Cjava2.java
@@ -0,0 +1,8 @@
+//My details 
+class Cjava2{
+	public static void main(String[] args) {
+	  System.out.println("Name: Akash Singh");     
+	  System.out.println("Course: B.TECH"); 
+    System.out.println("Stream: Computer Science Engineering");
+  }
+}
