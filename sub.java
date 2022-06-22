a97510edffa473580e8a403be0f17c621d1aa020
manduakash <50767686+manduakash@users.noreply.github.com>
2022-06-22 14:16:49 +0530

    Update sub.java

--- sub.java
+++ sub.java
@@ -1,12 +1,11 @@
+//SUBSTRACTION OF TWO NUMBERS
 class sub{
 	public static void main(String[] args) {
-	int a=10;
-	int b=5;
-            int c= a-b;
+	  int a=10; //1st number
+	  int b=5; //2nd number
+          int c= a-b; //Subtraction
+		
+	    //output
  	  System.out.println("a =" + a);
  	  System.out.println("b = " + b);
 	  System.out.println("Subtraction of A and B = " + c);
-	       
-	  
-  }
-}
