7714eb3ed1785af22cdc91f8230ae47be9c53377
manduakash <50767686+manduakash@users.noreply.github.com>
2022-06-22 14:09:14 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,9 +1,11 @@
 //ADDITION OF TWO NUMBERS
 class add{
 	public static void main(String[] args) {
-	int a=5;
-	int b=10;
-            int c= a+b;
+	  int a=5; //1st number
+	  int b=10; //2nd number
+          int c= a+b; //sum
+		
+	  //output	
  	  System.out.println("a =" + a);
  	  System.out.println("b = " + b);
 	  System.out.println("Addition of A and B = " + c);
