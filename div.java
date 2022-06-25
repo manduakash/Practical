8488e3672c43edf2d339e6bac630a55d9a5dc991
manduakash <50767686+manduakash@users.noreply.github.com>
2022-06-25 17:23:28 +0530

    Update div.java

--- div.java
+++ div.java
@@ -1,11 +1,25 @@
+//@Author AKASH SINGH
+
 //DIVITION OF TWO NUMBERS
+import javax.swing.*; //import for GUI dialog box
+
+
 class div{
+	//main method
 	public static void main(String[] args) {
-	  int a=10; //1st number
-	  int b=2; //2nd number
-          int c= a/b; //Divition
 		
+		JFrame jFrame = new JFrame(); //using jframe class for dialog boxes
+	    
+		String num1 = JOptionPane.showInputDialog(jFrame,"1st Number for divition"); //taking 1st number from user 
+		String num2 = JOptionPane.showInputDialog(jFrame,"2nd Number for divition"); //taking 2nd number from user 
+		
+		//changing input data type string to float
+		float num1F = Float.parseFloat(num1); 
+		float num2F = Float.parseFloat(num2); 
+		
+		float res = num1F/num2F; //logic
+	 	
 	    //output
- 	  System.out.println("a =" + a);
- 	  System.out.println("b = " + b);
-	  System.out.println("DIVITION of A and B = " + c);
+	    JOptionPane.showMessageDialog(jFrame,"Divition of " + num1 + " and " + num2 + " = " + res);
+	}
+}
