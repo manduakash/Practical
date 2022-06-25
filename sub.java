8b84880f390595a14f6ad9e2560f4dccc1663690
manduakash <50767686+manduakash@users.noreply.github.com>
2022-06-25 21:27:43 +0530

    Update sub.java

--- sub.java
+++ sub.java
@@ -1,11 +1,23 @@
+//@Author Akash Singh
 //SUBSTRACTION OF TWO NUMBERS
+
+import javax.swing.*; //importing GUI dialog box packages
+
 class sub{
+	//main method
 	public static void main(String[] args) {
-	  int a=10; //1st number
-	  int b=5; //2nd number
-          int c= a-b; //Subtraction
 		
-	    //output
- 	  System.out.println("a =" + a);
- 	  System.out.println("b = " + b);
-	  System.out.println("Subtraction of A and B = " + c);
+		JFrame jFrame = new JFrame(); // using JFrame class for GUI dialog boxes
+		
+		//taking input from user
+		String a = JOptionPane.showInputDialog(jFrame, "Tyepe 1st number for Substraction"); 
+		String b = JOptionPane.showInputDialog(jFrame, "Type 2nd number for Substraction"); 
+		
+		//changing data type of input variables
+		int a_Int = Integer.parseInt(a);
+		int b_Int = Integer.parseInt(b);
+		
+		//output
+		JOptionPane.showMessageDialog(jFrame,"Substraction of "+ a + " and "+ b + " = " + (a_Int-b_Int));
+	}
+}
