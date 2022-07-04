2791a2b15a6b3723dd748c542cd809a88b30fdd9
manduakash <50767686+manduakash@users.noreply.github.com>
2022-07-04 13:41:59 +0530

    Update ParaConstructor.java

--- ParaConstructor.java
+++ ParaConstructor.java
@@ -1,16 +1,20 @@
+//@Author Akash Singh
+//Java program to write parameterized constructor
 
 public class ParaConstructor {
-
 	String name;
 	
-	ParaConstructor(String name1){
+	ParaConstructor(String name1){ //creating a constructor 
 		name=name1;
 	}
-	public static void main(String[] args) {
-		ParaConstructor obj1 = new ParaConstructor("C++");
+	public static void main(String[] args) { //main method
+		
+		//creating objects
+		ParaConstructor obj1 = new ParaConstructor("C++"); 
 		ParaConstructor obj2 = new ParaConstructor("Java");
 		ParaConstructor obj3 = new ParaConstructor("Python");
 		
+		//Output
 		System.out.println(obj1.name);
 		System.out.println(obj2.name);
 		System.out.println(obj3.name);
