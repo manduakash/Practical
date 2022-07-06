55feca6b5f3a2766a147caf6858324c3276e9af0
manduakash <50767686+manduakash@users.noreply.github.com>
2022-07-06 11:07:10 +0530

    Create InheritanceCar.java

---
+++ InheritanceCar.java
@@ -0,0 +1,34 @@
+//@Author Akash Singh
+//An example of a Multi-level inheritance
+
+class Vehicle { // Vehicle is parent class of Car 
+	Vehicle(){ // Constructor
+		System.out.println("It is a vehicle");
+	}
+}
+
+class Car extends Vehicle { //Car is child class of Vehicle
+	Car(){ // Constructor
+		System.out.println("It is a Car also");
+	}
+}
+
+class Aulto extends Car { //Aulto is child class of Car class
+	Aulto(){ // Constructor
+		System.out.println("It is a Aulto"); 
+	}
+	void run() { 
+		System.out.println("Aulto is ready to run...");
+	}
+}
+
+public class InheritanceCar { // main class
+
+	public static void main(String[] args) { //main method
+		
+		Aulto c = new Aulto(); //creating an object of Aulto 
+		c.run();// output
+
+	}
+
+}
