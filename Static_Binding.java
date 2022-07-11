3539bd05f1a1bcddfb4af4a9535e80d2f225a8cf
manduakash <50767686+manduakash@users.noreply.github.com>
2022-07-11 14:09:01 +0530

    Create Static_Binding.java

---
+++ Static_Binding.java
@@ -0,0 +1,10 @@
+
+public class Static_Binding {
+private int cost = 500;
+    // main method
+	public static void main(String[] args) {
+		Static_Binding obj = new Static_Binding();
+		System.out.println(obj.cost);
+	}
+
+}
