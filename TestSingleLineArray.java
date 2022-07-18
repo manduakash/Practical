84381a8ef98bc99aaacb575a9d74c9c3c480722a
manduakash <50767686+manduakash@users.noreply.github.com>
2022-07-18 15:25:03 +0530

    Create TestSingleLineArray.java

---
+++ TestSingleLineArray.java
@@ -0,0 +1,14 @@
+
+public class TestSingleLineArray {	// class
+
+	public static void main(String[] args) { // main method
+		
+		int[] arr = {10,20,30,40,50,60,70}; 	// instantiating and initializing the array
+		
+		// traversing array
+		for(int i:arr) {		// enhanced for loop
+			System.out.println(i);
+		}
+	}
+
+}
