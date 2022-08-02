bc5034dbb96946c78062b9e634e6bfa1031d9c95
manduakash <50767686+manduakash@users.noreply.github.com>
2022-08-02 10:00:27 +0530

    Create SubStringEx.java

---
+++ SubStringEx.java
@@ -0,0 +1,11 @@
+package StringClass;
+
+public class SubStringEx {
+	public static void main(String[] args) {
+		String str = "Anudip Foundation, Skill and Carrer Developer Center";
+		String new_str = str.substring(7,24);
+		
+		System.out.println("old="+str);
+		System.out.println("new="+new_str);
+	}
+}
