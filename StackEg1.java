dc73243056369860497367cc6be3bd688200b4a7
manduakash <50767686+manduakash@users.noreply.github.com>
2022-08-19 12:47:58 +0530

    Create StackEg1.java

---
+++ StackEg1.java
@@ -0,0 +1,31 @@
+package collection;
+
+import java.util.Stack;
+
+public class StackEg1 {
+
+	public static void main(String[] args) {
+
+		Stack<Integer> s=new Stack<>();
+		boolean result =s.empty();
+		System.out.println("is empty?"+result);
+		s.push(25);
+		s.push(35);
+		s.push(40);
+		s.push(55);
+		s.push(80);
+		s.push(75);
+		System.out.println("stack elements"+s);
+		Integer digit=s.peek();
+		System.out.println("top element:"+digit);
+int digit1=s.search(35);
+System.out.println("search element:"+digit1);
+
+int x=s.size();
+System.out.println("size:"+x);
+result=s.empty();
+System.out.println("is empty?"+result);
+
+	}
+
+}
